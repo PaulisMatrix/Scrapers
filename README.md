8d67f99092e40f8dc2037be8769814f778a15288
PaulisMatrix <1999.yadwade@gmail.com>
2021-09-08 18:53:09 +0530

    Intial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+This is a dictionary bot created for displaying the meaning of a word when called by a specific command on the mentioned subreddit on Reddit website.
+
+For creating this bot I have used the following:
+1. PRAW (Python Reddit API Wrapper)
+2. requests
+3. Beautiful soup for webscrapping 
+4. schedule for scheduling the bot
+5. re for regex matching and much more...
+
+
+
+The syntax for triggering the bot is :
+shashi explain <your word> or !define <your word>
+
+I have implemented two word lookups.
+First is the call to the oxford dictionary api and if the word is not found there then
+second lookup is the urban dictionary which I have scrapped using beautiful soup.
+
+The bot returns the defintion,type of the word(noun/pronoun/adjective/verb) and an example 
+on a successful lookup else "Not found" message incase word is not found.
+
+Example :
+shashi exlpain mitigation
+
+Response:
+According to the Oxford dictionary:
+
+Your Word: mitigation.
+
+Word Type: noun.
+
+Meaning: the action of reducing the severity,seriousness, or painfulness of something.
+
+Example: the identification and mitigation of pollution.
+
+
---
+++ shashi.py
@@ -0,0 +1,231 @@
+# The entire code
+import re
+import os
+import schedule
+import requests
+import json
+import sys
+import pdb
+import praw
+import sys
+from datetime import datetime, timedelta
+import logging
+import random
+import time
+from bs4 import BeautifulSoup
+
+print("Starting configuration\n")
+
+Happy_Responses_Filename = "happy_responses.txt"
+Sad_Responses_Filename = "sad_responses.txt"
+# Extra_Responses_Filename="extra.txt"
+app_id = "b7e63fea"
+app_key = "b788c7faae8a903585bc41a11743acca"
+language = "en-gb"
+
+reddit = praw.Reddit(
+    client_id="<client_id>",
+    client_secret="<client_secret>",
+    user_agent="Shashi test v0.2",
+    username="Shaashiii",
+    password="<password>",
+)
+
+logging.basicConfig(
+    filename="logfile_shashi.log",
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+try:
+    with open(Happy_Responses_Filename, encoding="utf8") as f:
+        happy_responses = f.read().split("\n")
+except FileNotFoundError:
+    happy_responses = [":^)"]
+
+try:
+    with open(Sad_Responses_Filename, encoding="utf8") as f:
+        sad_responses = f.read().split("\n")
+except FileNotFoundError:
+    sad_responses = [":("]
+
+"""    
+try:
+    with open(Extra_Responses_Filename,encoding='utf8') as f:
+        extra_responses=f.read().split("\n")
+except FileNotFoundError:
+    extra_responses=['Well,fuck']
+"""
+
+replied_to = {}  # dictionary to store author and corresponding timestamp
+
+
+def restrict_user_spam():
+    now = datetime.now()
+    for author in list(replied_to.keys()):
+        timestamp = replied_to[author]
+        # print(replied_to)
+        if (now - timestamp) > timedelta(seconds=45):
+            replied_to.pop(author)
+            logging.info("%s can now request more words" % author)
+
+
+def reply_random(comment, responses):
+    reply_string = random.choice(responses).encode("utf8")
+    comment.upvote()
+    comment.reply(reply_string)
+    logging.info("Replied to u/%s with %s" % (comment.author, reply_string))
+
+
+def check_inbox():
+    # print("Checking inbox....\n")
+    for user_reply in reddit.inbox.unread(limit=None):
+        if isinstance(user_reply, praw.models.Comment):
+            print(user_reply.author)
+            if re.search("good", user_reply.body.lower(), re.IGNORECASE):
+                reply_random(user_reply, happy_responses)
+            elif re.search("bad", user_reply.body.lower(), re.IGNORECASE):
+                reply_random(user_reply, sad_responses)
+        user_reply.mark_read()
+    # print("Handled Replies\n")
+
+
+def search_comment(comment):
+    comment_body = comment.body.lower()
+    word = comment_body[comment_body.find("shashi explain") :].split(" ")[2]  # the word after shashi explain
+    print(f"Searching for {word}")
+    try:
+        user_word, user_meaning, user_example = word_lookup1(word)  # call oxford api
+        reply_string = (
+            "According to Oxford dictionary:\n\n"
+            + "**Your Word**: "
+            + str(word)
+            + "\n\n"
+            + "**Word Type**: "
+            + str(user_word)
+            + "\n\n"
+            + "**Meaning**: "
+            + str(user_meaning)
+            + "\n\n"
+        )
+        if "not found" not in user_example.lower():
+            reply_string += "**Example**: " + str(user_example) + "\n"
+        comment.reply(reply_string)
+        replied_to[comment.author] = datetime.now()
+    except Exception as oxford_exception:
+        print(oxford_exception)
+        user_meaning, user_example = word_lookup2(word)  # search in the urban dict
+        if (
+            user_meaning == "Definition Not Found"
+            and user_example == "Example Not Found"
+        ):
+            print("Word Not Found\n")
+            comment.reply(f"{word} Not Found!")
+            replied_to[comment.author] = datetime.now()
+        else:
+            reply_string = "According to Urban dictionary:\n\n"
+            if len(user_example) == 0:
+                reply_string += (
+                    "**Word**: "
+                    + str(word)
+                    + "\n\n"
+                    + "**Meaning**: "
+                    + str(user_meaning)
+                )
+            else:
+                reply_string += (
+                    "**Word**: "
+                    + str(word)
+                    + "\n\n"
+                    + "**Meaning**: "
+                    + str(user_meaning)
+                    + "\n\n"
+                    + "**Example**: "
+                    + str(user_example)
+                )
+            comment.reply(reply_string)
+            replied_to[comment.author] = datetime.now()
+
+
+def word_lookup1(word_id):
+    # import pdb; pdb.set_trace()
+    url = (
+        "https://od-api.oxforddictionaries.com:443/api/v2/entries/"
+        + language
+        + "/"
+        + word_id.lower()
+    )
+    r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+    json_data = json.loads(r.text)
+
+    word = json_data["results"][0]["lexicalEntries"][0]["lexicalCategory"]["id"]
+    meaning = json_data["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
+    example = json_data["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["examples"][0]["text"]
+    return word, meaning, example
+
+
+def lineBreaks(defin):
+    string = ""
+    ignore = False
+    for x in range(0, len(defin) - 4):
+        if defin[x] == "<":
+            ignore = True
+        if ignore == False:
+            string += defin[x]
+        if (
+            defin[x] + defin[x + 1] + defin[x + 2] + defin[x + 3] + defin[x + 4]
+            == "<br/>"
+        ):
+            string += "\n"
+        if defin[x] == ">":
+            ignore = False
+    return string
+
+
+def word_lookup2(word):
+    website = "https://www.urbandictionary.com/define.php?term=" + word
+
+    try:
+        source = requests.get(website).text
+        soup = BeautifulSoup(source, "lxml")
+        section = soup.find("div", class_="def-panel")
+        definition = section.find("div", class_="meaning")
+        example = section.find("div", class_="example")
+        string1 = lineBreaks(str(definition))
+        string2 = lineBreaks(str(example))
+        return string1.strip(), string2.strip()
+    except AttributeError as KE:
+        print(KE)
+        return "Definition Not Found", "Example Not Found"
+
+
+schedule.every(5).seconds.do(check_inbox)  # check inbox every 30 seconds
+schedule.every(5).seconds.do(restrict_user_spam)  # to avoid spamming
+
+# comment_stream=reddit.subreddit("GumTest").stream.comments
+subreddit = reddit.subreddit("GumTest")
+
+print("Starting the stream loop...\n")
+
+while True:
+    try:
+        for comment in subreddit.stream.comments(skip_existing=True):
+            # print(comment.body,comment.author)
+            schedule.run_pending()
+            if comment.author not in replied_to:
+                if "shashi explain" in comment.body.lower():
+                    comment.upvote()
+                    # print(comment.body)
+                    search_comment(comment)
+    except praw.exceptions.APIException as e:
+        logging.warn(str(e))
+        logging.warn("Rate Limit Exceeded. Sleeping for a minute.")
+        time.sleep(60)
+    except KeyboardInterrupt:
+        print("User interrupted the script\n")
+        sys.exit()
+    except Exception as e:
+        logging.exception(e)
+        logging.error("Any miscellanous error,Sleeping for 10 seconds")
+        time.sleep(10)
