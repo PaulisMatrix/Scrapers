4fc9f21ee8333b7643804192c709914f6fbf9e80
PaulisMatrix <1999.yadwade@gmail.com>
2022-12-17 20:15:36 +0530

    adding linting/formatting

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def ping():
     return "Hello There!!"
 
 
+@app.route("/testing")
+def testing():
+    return {"data":"data1","data2":"data2","data3":"data3","data4":"data4","data5":"data5","data6":"data6"}
+
+
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=3000)
