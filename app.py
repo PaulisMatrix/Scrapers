87be087f112b1fbf05c13ad6029d26b43ef2bf6c
PaulisMatrix <1999.yadwade@gmail.com>
2022-12-17 20:47:28 +0530

    add precommit hooks

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def ping():
     return "Hello There!!"
 
 
+@app.route("/testing")
+def testing():
+    return {"data1":"data1","data2":"data2","data3":"data3","data4":"data4","data5":"data5","data6":"data6","data7":"data7 "}
+
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=3000)
